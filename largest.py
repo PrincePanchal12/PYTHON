7491aa773d6dcadc04c8b2a226e553fd2c2ddd11
Prince Panchal <113456053+PrincePanchal12@users.noreply.github.com>
2024-10-04 22:57:43 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,18 @@
+num_1= int(input("Enter number 1: "))
+num_2= int(input("Enter number 2: "))
+num_3= int(input("Enter number 3: "))
+
+if num_1 > num_2 :
+    if num_1>num_3:
+        print("largest number is ",num_1)
+    else:
+        print("largest number is ",num_3)
+else:
+        if num_2>num_3:
+             print("largest number is ",num_2)
+        else:
+            print("largest number is ",num_3)
+
+    
+        
+ 
